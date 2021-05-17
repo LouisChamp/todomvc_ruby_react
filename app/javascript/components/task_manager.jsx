30a291ab0f1b4b0727ecfdac10f3c22626f8aa17
Cassiano D'Andrea <cassiano@Cassianos-MacBook-Air.local>
2021-05-17 20:15:23 -0300

    WIP

--- app/javascript/components/task_manager.jsx
+++ app/javascript/components/task_manager.jsx
@@ -1,63 +1,70 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-const TaskManager = props => (
-  <>
-  	<section className="todoapp">
-  		<header className="header">
-  			<h1>todos</h1>
-  			<input className="new-todo" placeholder="What needs to be done?" autoFocus />
-  		</header>
+const TaskManager = props => {
+	const tasks = [
+		{ id: 1, title: 'Task A', completed: true },
+		{ id: 2, title: 'Task B', completed: false },
+		{ id: 3, title: 'Task C', completed: true },
+	]
 
-  		<section className="main">
-  			<input id="toggle-all" className="toggle-all" type="checkbox" />
-  			<label htmlFor="toggle-all">Mark all as complete</label>
-  			<ul className="todo-list">
-  				<li className="completed">
-  					<div className="view">
-  						<input className="toggle" type="checkbox" checked onChange={() => {}} />
-  						<label>Taste JavaScript</label>
-  						<button className="destroy"></button>
-  					</div>
-  					<input className="edit" value="Create a TodoMVC template" onChange={() => {}} />
-  				</li>
-  				<li>
-  					<div className="view">
-  						<input className="toggle" type="checkbox" />
-  						<label>Buy a unicorn</label>
-  						<button className="destroy"></button>
-  					</div>
-  					<input className="edit" value="Rule the web"  onChange={() => {}} />
-  				</li>
-  			</ul>
-  		</section>
+	const pendingTasks = tasks.filter(task => !task.completed)
 
-  		<footer className="footer">
-  			<span className="todo-count"><strong>0</strong> item left</span>
-  			<ul className="filters">
-  				<li>
-  					<a className="selected" href="#/">All</a>
-  				</li>
-  				<li>
-  					<a href="#/active">Active</a>
-  				</li>
-  				<li>
-  					<a href="#/completed">Completed</a>
-  				</li>
-  			</ul>
-  			<button className="clear-completed">Clear completed</button>
-  		</footer>
-  	</section>
+	return (
+		<>
+			<section className="todoapp">
+				<header className="header">
+					<h1>todos</h1>
+					<input className="new-todo" placeholder="What needs to be done?" autoFocus />
+				</header>
 
-  	<footer className="info">
-  		<p>Double-click to edit a todo</p>
-        <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
-  		<p>Created by <a href="http://todomvc.com">you</a></p>
-  		<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-  	</footer>
-  </>
-)
+				<section className="main">
+					<input id="toggle-all" className="toggle-all" type="checkbox" />
+					<label htmlFor="toggle-all">Mark all as complete</label>
+					<ul className="todo-list">
+						{tasks.map(task => (
+							<li className={classNames({ completed: task.completed })} key={task.id}>
+								<div className="view">
+									<input className="toggle" type="checkbox" checked={task.completed} onChange={() => { }} />
+									<label>{task.title}</label>
+									<button className="destroy"></button>
+								</div>
+								<input className="edit" value="Create a TodoMVC template" onChange={() => { }} />
+							</li>
+						))}
+					</ul>
+				</section>
+
+				<footer className="footer">
+					<span className="todo-count"><strong>{pendingTasks.length}</strong> item{
+						pendingTasks.length === 1 ? '' : 's'
+					} left</span>
+					<ul className="filters">
+						<li>
+							<a className="selected" href="#/">All</a>
+						</li>
+						<li>
+							<a href="#/active">Active</a>
+						</li>
+						<li>
+							<a href="#/completed">Completed</a>
+						</li>
+					</ul>
+					<button className="clear-completed">Clear completed</button>
+				</footer>
+			</section>
+
+			<footer className="info">
+				<p>Double-click to edit a todo</p>
+				<p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+				<p>Created by <a href="http://todomvc.com">you</a></p>
+				<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+			</footer>
+		</>
+	)
+}
 
 TaskManager.defaultProps = {
 }

43d1b0187b8be1769294ab99694988d7a63275b4
Luiz Rocco <luizgustavorocco@gmail.com>
2021-05-20 14:02:20 -0400

    modified React jsx

--- app/javascript/components/task_manager.jsx
+++ app/javascript/components/task_manager.jsx
@@ -1,85 +1,94 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import className from 'classnames'
 import classNames from 'classnames'
 import axios from 'axios'
 
 const TaskManager = props => {
-	const [tasks, setTasks] = useState([])
+	
+		const [tasks, setTasks] = useState([])
 
-	useEffect(() => {
-		axios.get('/api/v1/tasks')
+		useEffect(() => {
+			axios.get('api/v1/tasks')
 			.then(response => {
 				setTasks(response.data)
 			})
 			.catch(console.log)
-	}, [])
+		}, [])
 
-	const pendingTasks = tasks.filter(task => !task.completed)
+		const tasksIncomplete = tasks.filter(task => !task.completed)
 
-	const handleTaskDestroy = task => event => {
-		if (confirm(`Are you sure you want to delete task '${task.title}'?`)) {
+		const handleTaskDestroy = task => event => {
+			if(confirm(`Are you sure you want to delete task ${task.title}`))
 			axios.delete(`/api/v1/tasks/${task.id}`)
-				.then(response => {
-					setTasks(
-						tasks.filter(tsk => tsk.id !== task.id)
-					)
-				})
-				.catch(console.log)
+			.then(response => {
+				setTasks(
+					tasks.filter(tsk => tsk.id !== task.id)
+				)
+			})
+			.catch(console.log)
 		}
-	}
+
+		const handleToggleCheck = id => {
+			
+		}
+
+		// const handleEnterPress = () => {
+		// 	axios.
+		// }
 
 	return (
-		<>
-			<section className="todoapp">
-				<header className="header">
-					<h1>todos</h1>
-					<input className="new-todo" placeholder="What needs to be done?" autoFocus />
-				</header>
+  <>
+  	<section className="todoapp">
+  		<header className="header">
+  			<h1>todos</h1>
+  			<input className="new-todo" placeholder="What needs to be done?" autoFocus onKeyUp={handleEnterPress()}/>
+  		</header>
 
-				<section className="main">
-					<input id="toggle-all" className="toggle-all" type="checkbox" />
-					<label htmlFor="toggle-all">Mark all as complete</label>
-					<ul className="todo-list">
-						{tasks.map(task => (
-							<li className={classNames({ completed: task.completed })} key={task.id}>
-								<div className="view">
-									<input className="toggle" type="checkbox" checked={task.completed} onChange={() => { }} />
-									<label>{task.title}</label>
-									<button className="destroy" onClick={handleTaskDestroy(task)}></button>
-								</div>
-								<input className="edit" value="Create a TodoMVC template" onChange={() => { }} />
-							</li>
-						))}
-					</ul>
-				</section>
+  		<section className="main">
+  			<input id="toggle-all" className="toggle-all" type="checkbox" />
+  			<label htmlFor="toggle-all">Mark all as complete</label>
+  			<ul className="todo-list">
+					{
+						tasks.map(task => (
+  				<li className={classNames({completed: task.completed})} key={task.id}>
+  					<div className="view">
+  						<input className="toggle" type="checkbox" checked onChange={() => handleToggleCheck(task.id)} />
+  						<label>{task.title}</label>
+  						<button className="destroy" onClick={ handleTaskDestroy(task)}></button>
+  					</div>
+  					<input className="edit" value="Create a TodoMVC template" onChange={() => {}} />
+  				</li>
+						))
+					}
+  			</ul>
+  		</section>
 
-				<footer className="footer">
-					<span className="todo-count"><strong>{pendingTasks.length}</strong> item{
-						pendingTasks.length === 1 ? '' : 's'
-					} left</span>
-					<ul className="filters">
-						<li>
-							<a className="selected" href="#/">All</a>
-						</li>
-						<li>
-							<a href="#/active">Active</a>
-						</li>
-						<li>
-							<a href="#/completed">Completed</a>
-						</li>
-					</ul>
-					<button className="clear-completed">Clear completed</button>
-				</footer>
-			</section>
+  		<footer className="footer">
+  			<span className="todo-count"><strong>{tasksIncomplete.length}</strong> item{tasksIncomplete.length === 1 ? '' : 's'} left</span>
+  			<ul className="filters">
+  				<li>
+  					<a className="selected" href="#/">All</a>
+  				</li>
+  				<li>
+  					<a href="#/active">Active</a>
+  				</li>
+  				<li>
+  					<a href="#/completed">Completed</a>
+  				</li>
+  			</ul>
+  			<button className="clear-completed">Clear completed</button>
+  		</footer>
+  	</section>
 
-			<footer className="info">
-				<p>Double-click to edit a todo</p>
-				<p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
-				<p>Created by <a href="http://todomvc.com">you</a></p>
-				<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-			</footer>
-		</>
+  	<footer className="info">
+  		<p>Double-click to edit a todo</p>
+        <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+  		<p>Created by <a href="http://todomvc.com">you</a></p>
+  		<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+  	</footer>
+  </>
 	)
 }
 
